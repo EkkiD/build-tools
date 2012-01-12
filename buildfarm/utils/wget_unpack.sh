a4a934ea54dd5d74d23a834198f2158c3d1ac1e4
Erick Dransch <edransch@mozilla.com>
2012-01-11 21:08:02 -0500

    check both results in wget_unpack

--- buildfarm/utils/wget_unpack.sh
+++ buildfarm/utils/wget_unpack.sh
@@ -6,13 +6,16 @@
 
 wget -O wget_unpack.tar.gz ${1}/${2}
 
+getResult=$?
+
 tar -xzvf wget_unpack.tar.gz
+
+unpackResult=$?
 
 # if these steps succeed, add ".old" to the suffixes
 # if these steps fail, go get the files individually.
 
-result=$?
-if [ $result -eq 0 ]; then
+if [ $getResult -eq 0 ] && [$unpackResult -eq 0]; then
     echo "Got the packed files"
     for ((i=3 ; i <= $# ; i++ )); do
         echo "mv ./${!i} ./${!i}.old"

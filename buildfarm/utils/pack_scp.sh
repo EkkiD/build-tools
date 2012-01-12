e923b979d7b4780aaa4508eac121d07df6fd8574
Erick Dransch <edransch@mozilla.com>
2012-01-12 10:37:04 -0500

    make tar handle '../' correctly

--- buildfarm/utils/pack_scp.sh
+++ buildfarm/utils/pack_scp.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # $1 : name of the compressed file
-# $2 : files to compress
-# $3 : username
-# $4 : ssh key file
-# $5 : target location
+# $2 : directory to move to
+# $3 : files to compress
+# $4 : username
+# $5 : ssh key file
+# $6 : target location
 
-echo "tar -czf ${1} ${2}"
-tar -czf $1 $2
+echo "tar -cz -C ${2} -f ${1} ${3}"
+tar -cz -C $2 -f $1 $3
 
-echo "scp -o User=$3 -o IdentityFile=~/.ssh/$4 $1 $5"
-scp -o User=$3 -o IdentityFile=~/.ssh/$4 $1 $5
+echo "scp -o User=$4 -o IdentityFile=~/.ssh/$5 $1 $6"
+scp -o User=$4 -o IdentityFile=~/.ssh/$5 $1 $6
 
 rm $1

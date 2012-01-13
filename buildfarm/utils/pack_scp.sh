00b60bad0fcc885da8b70d5534a1ed474068eba1
Erick Dransch <edransch@mozilla.com>
2012-01-13 11:16:04 -0500

    missing change from previous commit

--- buildfarm/utils/pack_scp.sh
+++ buildfarm/utils/pack_scp.sh
@@ -13,6 +13,6 @@ echo "tar -cz -C ${2} -f ${1} ${3}"
 tar -cz -C $2 -f $1 $3
 
 echo "scp -o User=$4 -o IdentityFile=$HOME/.ssh/$5 $1 $6"
-scp -o User=$4 -o IdentityFile=~/.ssh/$5 $1 $6
+scp -o User=$4 -o IdentityFile=$HOME/.ssh/$5 $1 $6
 
 rm $1

99b3e106198dee5edc55c55d1969957e05e2a5a2
Erick Dransch <edransch@mozilla.com>
2012-01-19 17:43:17 -0500

    remove retry

--- buildfarm/utils/pack_scp.sh
+++ buildfarm/utils/pack_scp.sh
@@ -48,7 +48,7 @@ fi
 echo "tar -cz -C ${2} -f ${1} ${3}"
 tar -cz -C $2 -f $1 $3
 
-echo " ${retry} scp -o User=$4 -o IdentityFile=\"${HOME}/.ssh/$5\" $1 $6"
-${retry} scp -o User=$4 -o IdentityFile="${HOME}/.ssh/$5" $1 $6
+echo " scp -o User=$4 -o IdentityFile=\"${HOME}/.ssh/$5\" $1 $6"
+scp -o User=$4 -o IdentityFile="\"${HOME}/.ssh/$5\"" $1 $6
 
 rm $1
